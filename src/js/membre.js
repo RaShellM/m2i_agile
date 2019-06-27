7a54a74331e8889bbec507a86104e7bc5ea0cef7
RaShell <racheloumartin@gmail.com>
2019-06-27 13:46:21 +0200

    ajout membres

---
+++ src/js/membre.js
@@ -0,0 +1,9 @@
+$(function(){
+
+	$('button').on('click',function() {
+    $('#essaiAnim').animate({
+                left: '700px',
+                width: '+=200px',
+                height: '+=200px' },2000);
+ 	});
+ });
