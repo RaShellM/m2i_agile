657995d09086abe199c5a8c3ee916f4a19a1898f
RaShell <racheloumartin@gmail.com>
2019-06-28 13:34:00 +0200

    ajout photos

--- src/js/membre.js
+++ src/js/membre.js
@@ -10,6 +10,16 @@ $(function(){
 
     $('#tech').on('click', function() {
     	$('#technologies').toggle();
-    	  });	
+    	 });	
+
+    $('.moove').mouseover(function() {
+  		$(this).animate({ "bottom": "+=15px" }, 300 );
+ 		$(this).animate({ "bottom": "-=15px" }, 200 );
+ 		$(this).animate({ "bottom": "+=8px" }, 90 );
+ 		$(this).animate({ "bottom": "-=8px" }, 90 );
+ 		$(this).animate({ "bottom": "+=2px" }, 80 );
+ 		$(this).animate({ "bottom": "-=2px" }, 80 );
+	});
+
 
 });
